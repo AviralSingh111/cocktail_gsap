56b26e46994b2901b36d900007a5f346520ac1f9
Aviral Singh <aviralsingh360@gmail.com>
2026-04-26 14:57:44 +0530

    Remaining Sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-import gsap from "gsap";
+import gsap from 'gsap';
 import { ScrollTrigger, SplitText } from "gsap/all";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+
+import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
+import Cocktails from './components/Cocktails.jsx'
+import About from './components/About.jsx'
+import Art from './components/Art.jsx'
+import Menu from './components/Menu.jsx'
+import Contact from './components/Contact.jsx'
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
@@ -10,9 +16,13 @@ const App = () => {
     <main>
       <Navbar />
       <Hero />
-      <div className="h-dvh bg-black"></div>
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact />
     </main>
-  );
-};
+  )
+}
 
-export default App;
+export default App
